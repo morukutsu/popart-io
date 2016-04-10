abbc74dc447172e0fa6da27afcd04b7a66d8831e
Morukutsu <ulderic.k@free.fr>
2016-04-10 02:14:41 +0200

    [feature] added effect class from old popart io project

--- src/pages/login/page.js
+++ src/pages/login/page.js
@@ -3,33 +3,39 @@ import { browserHistory } from 'react-router';
 import styles from './style.css';
 import GL from 'gl-react';
 import { Surface } from 'gl-react-dom';
- 
- const shaders = GL.Shaders.create({
-  helloGL: {
-    frag: `
-precision highp float;
-varying vec2 uv;
-uniform float blue;
-void main () {
-  gl_FragColor = vec4(uv.x, uv.y, blue, 1.0);
-}`
-  }
-});
+import { Effect, EffectCore }          from '../../popart/Effect';
 
 export default class LoginPage extends React.Component {
-    /*signUp() {
-        browserHistory.push('/home');
-    }*/
-    
+    constructor() {
+        super();
+        this.update = this.update.bind(this); // binding
+    }
+
+    componentWillMount() {
+        this.ec = new EffectCore();
+        this.update();
+    }
+
+    update() {
+        this.ec.tick(0.016);
+        this.raf = window.requestAnimationFrame(this.update);
+
+        // Trigger renger
+        this.setState({
+            dummy: 1
+        });
+    }
+
+    componentWillUnmount () {
+        cancelAnimationFrame(this.raf);
+    }
+
     render() {
         return (
             <div className={styles.content}>
                 hello world
                 <Surface width={511} height={341}>
-                    <GL.Node
-                        shader={shaders.helloGL}
-                        uniforms={{blue: 0.5}}
-                      />
+                    <Effect state={this.ec.getState() }/>
                 </Surface>
             </div>
         );
---
+++ src/popart/Effect.js
@@ -0,0 +1,41 @@
+import React, { Component, PropTypes } from 'react';
+import GL                              from 'gl-react';
+
+const shaders = GL.Shaders.create({
+    helloGL: {
+        frag: `
+        precision highp float;
+        varying vec2 uv;
+        uniform float blue;
+        void main () {
+            gl_FragColor = vec4(uv.x, uv.y, blue, 1.0);
+        }`
+    }
+});
+
+export class EffectCore {
+    constructor() {
+        this.blue = 1.0;
+        this.time = 0.0;
+    }
+
+    tick(dt) {
+        this.blue = Math.abs(Math.sin(this.time));
+        this.time += dt * 2;
+    }
+
+    getState() {
+        return this;
+    }
+}
+
+export const Effect = GL.createComponent(({ state }) => {
+    return (
+        <GL.Node
+            shader={shaders.helloGL}
+            uniforms={{blue: state.blue}}
+        />
+    );
+}, {
+  displayName: "Effect"
+});
